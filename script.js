b5825d19b7235b71f8069c71e013f80a2762e5e1
myradio <154856702+pristaaaa@users.noreply.github.com>
2024-01-27 21:35:24 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function getMPDFile() {
+  return "https://bpprod4linear.akamaized.net/bpk-tv/irdeto_com_Channel_935/output/manifest.mpd";
+}
+
+function getClearKey() {
+  return "9c37476f7d577509aa6af2f5ce09c917";
+}
+
+function getClearKeyId() {
+  return "455d533d69f35c35aa0853e6212bc2e7";
+}
